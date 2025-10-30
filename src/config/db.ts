ace2c7db10682c5e0fdd8e33ac6c1ccda84ada01
“LienThuan9697” <lienthuan2004@gmail.com>
2025-10-30 22:47:58 +0700

    edit readme

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,10 +3,10 @@ import "dotenv/config";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.URL_MONGO || "");
+    await mongoose.connect(process.env.URL_MONGO || ""); // Connect to MongoDB using the connection string from environment variables
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1);// Exit process with failure
+    process.exit(1);// Exit process with failure, with code 1 indicating an error and successful connection is 0
   }
 };
