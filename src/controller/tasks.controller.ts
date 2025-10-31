e4958b5cf44bd7c8c162a9fe6290a031ec0b59be
“LienThuan9697” <lienthuan2004@gmail.com>
2025-10-31 12:36:29 +0700

    tạo RESTFUL API cho backend với models và sửa lại 1 số thứ bên trong

--- src/controller/tasks.controller.ts
+++ src/controller/tasks.controller.ts
@@ -1,19 +1,65 @@
 import { Request, Response } from "express";
+import Task from "models/Task.models";
 
-export const getAllTasks = (req: Request, res: Response) => {
-  res.status(200).json({ message: "you have 20 tasks need to be completed", data: [] });
+export const getAllTasks = async (req: Request, res: Response) => {
+  // res.status(200).json({ message: "you have 20 tasks need to be completed", data: [] });
+  try {
+    const tasks = await Task.find().sort({ createdAt: 'desc' }); // Retrieve all tasks from the database, sorted by creation date in descending order
+    if (tasks.length === 0) {
+      return res.status(200).json({ message: "No tasks found", data: [] });
+    }
+    res.status(200).json({ message: "Tasks retrieved successfully", data: tasks });
+  } catch (error) {
+    console.error("Error Get All tasks:", error);
+    res.status(500).json({ message: "Error retrieving tasks", error: error });
+  }
 };
 
-export const createTask = (req: Request, res: Response) => {
-  res.status(201).json({ message: "Task created successfully", data: req.body });
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+    const newTask = new Task({ title, description });// Create a new Task instance with the title and description from the request body
+    const savedTask = await newTask.save(); // Save the new task to the database
+    res.status(201).json({ message: "Task created successfully", data: savedTask });
+  } catch (error) {
+    console.error("Error creating task:", error);
+    res.status(500).json({ message: "Error creating task", error: error });
+  }
 };
 
-export const updateTask = (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.status(200).json({ message: `Task with id ${id} updated successfully`, data: req.body });
+export const updateTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { title, description, status, completedAt } = req.body;
+    const updatedTask = await Task.findByIdAndUpdate(
+      String(id),
+      { title, description, status, completedAt },
+      { new: true },// Return value the updated document, not the value original. if set to false, return the document as it was before the update was applied.
+    );
+    if (!updatedTask) {
+      return res.status(404).json({ message: `Task with id ${id} not found` });
+    } else {
+      return res.status(200).json({ message: "Task updated successfully", data: updatedTask });
+    }
+
+  } catch (error) {
+    console.error("Error updating task:", error);
+    return res.status(500).json({ message: "Error updating task", error: error });
+  }
 };
 
-export const deleteTask = (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.status(200).json({ message: `Task with id ${id} deleted successfully` });
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(String(id));
+    if (!deletedTask) {
+      return res.status(404).json({ message: `Task with id ${id} not found` });
+    } else {
+      return res.status(200).json({ message: "Task deleted successfully", data: deletedTask });
+      
+    }
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    return res.status(500).json({ message: "Error deleting task", error: error });
+  }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import routes from "routes/tasksRouters";
-import { connectDB } from "config/DB";
+import { connectDB } from "config/db";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,15 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to the database
-connectDB();
-
 // Importing the routes
 routes(app);
 
-// Starting the server
-app.listen(PORT, () => {
-  console.log(`Server is starting...: ${__dirname}`);
-  const url = `http://localhost:${PORT}`;
-console.log(`Server is running on PORT: \x1b[32m\u001b]8;;${url}\u0007${url}\u001b]8;;\u0007\x1b[0m`);
+// Connect to the database
+connectDB().then(() => {
+  // Starting the server after successful DB connected
+  app.listen(PORT, () => {
+    console.log(`Server is starting...: ${__dirname}`);
+    const url = `http://localhost:${PORT}`;
+    console.log(`Server is running on PORT: \x1b[32m\u001b]8;;${url}\u0007${url}\u001b]8;;\u0007\x1b[0m`);
+  });
 });
